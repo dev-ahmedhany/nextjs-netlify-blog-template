6f0afc32033f6bb340d7d0f28ac3eff82063bc79
Ahmed Hany <dev.ahmedhany@gmail.com>
2021-04-18 12:57:52 +0200

    yml tweet file

--- src/lib/tweets.ts
+++ src/lib/tweets.ts
@@ -1,3 +1,26 @@
-import tweets from "../../content/tweets.json";
+import tweets from "../../content/tweets.yml";
 
-export default tweets as string[];
+
+export type TweetContent = {
+  readonly id: string;
+};
+
+const tagMap: { [key: string]: TweetContent } = generateTagMap();
+
+function generateTagMap(): { [key: string]: TweetContent } {
+  let result: { [key: string]: TweetContent } = {};
+  for (const tag of tweets.tweets) {
+    result[tag.id] = tag;
+  }
+  return result;
+}
+
+export function getTag(id: string) {
+  return tagMap[id];
+}
+
+export function listTags(): TweetContent[] {
+  return tweets.tweets;
+}
+
+export default tweets.tweets;
