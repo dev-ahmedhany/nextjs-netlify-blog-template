46be25a517c10b067a719f029d28a41611a7210c
Ahmed Hany <dev.ahmedhany@gmail.com>
2021-04-18 13:06:07 +0200

    delete unnesseary code

--- src/lib/tweets.ts
+++ src/lib/tweets.ts
@@ -1,26 +1,3 @@
 import tweets from "../../content/tweets.yml";
 
-
-export type TweetContent = {
-  readonly id: string;
-};
-
-const tagMap: { [key: string]: TweetContent } = generateTagMap();
-
-function generateTagMap(): { [key: string]: TweetContent } {
-  let result: { [key: string]: TweetContent } = {};
-  for (const tag of tweets.tweets) {
-    result[tag.id] = tag;
-  }
-  return result;
-}
-
-export function getTag(id: string) {
-  return tagMap[id];
-}
-
-export function listTags(): TweetContent[] {
-  return tweets.tweets;
-}
-
 export default tweets.tweets;
