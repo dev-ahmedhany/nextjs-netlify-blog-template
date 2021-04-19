36efcc878b506163fcd8d279aec4a2fff1b764b2
Ahmed Hany <dev.ahmedhany@gmail.com>
2021-04-19 11:46:40 +0200

    tweets Loading

--- src/components/RandomTweet/tweets.js
+++ src/components/RandomTweet/tweets.js
@@ -1,21 +1,46 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import { Tweet } from "react-twitter-widgets";
 import shuffle from "shuffle-array";
 import ids from "../../lib/tweets";
+const tweetsarray = shuffle(ids);
+import Loader from 'react-spinners/HashLoader'
 
-class Tweets extends React.Component {
-  state = {
-    tweets: shuffle(ids)
-  };
-  render() {
-    return (
-    <>
-        {this.state.tweets.map(id => (
-        <Tweet key={id} tweetId={id} options={{ conversation: "none" }} />
-        ))}
-    </>
-    );
-  }
+const Tweets = () => {
+  const [newTweets,setNewTweets] = useState([]);
+  const [currentCount, setCount] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const override = `
+  display: block;
+  margin: 0 auto;
+`;
+  useEffect(() =>{
+    setNewTweets( [...newTweets,tweetsarray[currentCount]])
+    const up = ()=>
+    {
+      if(currentCount + 1 !== tweetsarray.length)
+      {
+        setCount(currentCount +1)
+      }
+      else{
+        setLoading(false)
+      }
+    }
+    const interval = setTimeout(up, 1500);
+    return () => clearTimeout(interval);
+  },[currentCount]);
+  
+  return (
+  <>
+      {newTweets.map(id => (
+      <Tweet key={id} tweetId={id} options={{ conversation: "none" }} />
+      ))}
+      <Loader color="#1da0f2" //twitter blue
+      css={override} 
+      loading={loading}
+       size={200}/>
+  </>
+  );
+  
 }
 
 export default Tweets;
